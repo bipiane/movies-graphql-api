8723194ce64a793a542611cf39a00eca9682f38c
Ivan Pianetti <ivanpianetti23@gmail.com>
2018-07-10 13:39:48 -0300

    Config Express-GraphQL

--- app.js
+++ app.js
@@ -3,4 +3,12 @@
 const express = require('express');
 const app = express();
 
+const graphqlHTTP = require('express-graphql');
+const schema = require('./graphql/schema');
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
 module.exports = app;
